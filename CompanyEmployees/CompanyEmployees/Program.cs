289160487d6c3c1412670fd1a47b6342945c4902
= <=>
2024-05-21 11:57:51 -0700

    [feature] : Supported CORS and IIS integration

--- CompanyEmployees/CompanyEmployees/Program.cs
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -1,6 +1,9 @@
+using CompanyEmployees.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 
