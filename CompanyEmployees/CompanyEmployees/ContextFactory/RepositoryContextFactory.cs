5d2b4187196cb94d58b1b583d62245367fca5783
= <=>
2024-05-25 12:37:51 -0700

    [feature] Created RepositoryContextFactory

---
+++ CompanyEmployees/CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace CompanyEmployees.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlServer(configuration.GetConnectionString("sqlConnection"));
+
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
