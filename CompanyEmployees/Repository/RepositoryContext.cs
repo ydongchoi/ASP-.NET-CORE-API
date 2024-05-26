34c52452bd929a53e49b3e627d5d2c4ce3ca2f04
= <=>
2024-05-26 14:43:01 -0700

    [feature] Invoked Configuration.

--- CompanyEmployees/Repository/RepositoryContext.cs
+++ CompanyEmployees/Repository/RepositoryContext.cs
@@ -1,5 +1,6 @@
 ﻿using Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Repository.Configuration;
 
 namespace Repository
 {
@@ -8,6 +9,12 @@ namespace Repository
         public RepositoryContext(DbContextOptions options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
         }
 
         public DbSet<Company>? Companies { get; set; }
