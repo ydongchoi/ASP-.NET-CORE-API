f44601b97c03c40478d031b1ab197f4e70ae6500
= <=>
2024-05-26 14:35:16 -0700

    [feature] Created CompanyConfiguration for populating Table with Initial Data.

---
+++ CompanyEmployees/Repository/Configuration/CompanyConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Configuration
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.HasData(
+                new Company
+                {
+                    Id = new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"),
+                    Name = "IT_Solution_Ltd",
+                    Address = "583 Wall Dr, Gwyn Oak, MD 21207",
+                    Country = "USA"
+                },
+                new Company
+                {
+                    Id = new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3"),
+                    Name = "Admin_Solution_Ltd",
+                    Address = "312 Forest Avenue, BF 923",
+                    Country = "USA"
+                }
+            );
+        }
+    }
+}
