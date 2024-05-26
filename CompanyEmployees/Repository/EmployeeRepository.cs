df5594d344e68ff8280cc9fdd9f7c0761f7afd11
= <=>
2024-05-26 15:33:34 -0700

    [feature] Created EmployeeRepository Class.

---
+++ CompanyEmployees/Repository/EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+    }
+}
