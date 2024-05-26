b7f2ebf579f7d888335ce0b59cabd2fb2bbfb9bc
= <=>
2024-05-26 15:17:18 -0700

    [feature] Created Generic RepositoryBase.

---
+++ CompanyEmployees/Repository/RepositoryBase.cs
@@ -0,0 +1,31 @@
+﻿using Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext RepositoryContext;
+
+        protected RepositoryBase(RepositoryContext repositoryContext) => RepositoryContext = repositoryContext;
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ? RepositoryContext.Set<T>().AsNoTracking() : RepositoryContext.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) =>
+            !trackChanges ? RepositoryContext.Set<T>().Where(expression).AsNoTracking() :
+            RepositoryContext.Set<T>().Where(expression);
+
+        public void Create(T entity) => RepositoryContext.Set<T>().Add(entity);
+
+        public void Update(T entity) => RepositoryContext.Set<T>().Update(entity);
+
+        public void Delete(T entity) => RepositoryContext.Set<T>().Remove(entity);
+    }
+}
