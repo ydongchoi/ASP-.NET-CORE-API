494a2e5270ced7acabece49e6aa58f975c92cf65
= <=>
2024-05-26 15:31:58 -0700

    [feature] Created CompanyRepository Class.

---
+++ CompanyEmployees/Repository/CompanyRepository.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {            
+        }
+    }
+}
