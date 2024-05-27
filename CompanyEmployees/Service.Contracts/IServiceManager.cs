6937172821d5b5c19a3928f45798a31321b0302f
= <=>
2024-05-27 15:26:17 -0700

    [feature] Created Company, Employee, ServiceManager Interface

---
+++ CompanyEmployees/Service.Contracts/IServiceManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Contracts
+{
+    public interface IServiceManager
+    {
+        ICompanyService CompanyService { get; }
+        IEmployeeService EmployeeService { get; }
+    }
+}
