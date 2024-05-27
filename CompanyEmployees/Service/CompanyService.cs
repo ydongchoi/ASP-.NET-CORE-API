c586bc5bfe7f7ce9cd757da767e8500731df8237
= <=>
2024-05-27 15:30:17 -0700

    [feature] Created CompanyService Class in Service.

---
+++ CompanyEmployees/Service/CompanyService.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using Service.Contracts;
+
+namespace Service
+{
+    internal sealed class CompanyService : ICompanyService
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+
+        public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+    }
+}
