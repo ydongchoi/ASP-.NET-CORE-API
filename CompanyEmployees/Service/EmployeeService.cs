8991620ef006a74d8ba3299e6423a78928d9d56f
= <=>
2024-05-27 15:32:27 -0700

    [feature] Created Employee Service

---
+++ CompanyEmployees/Service/EmployeeService.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    internal sealed class EmployeeService : IEmployeeService
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+
+        public EmployeeService(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+    }
+}
