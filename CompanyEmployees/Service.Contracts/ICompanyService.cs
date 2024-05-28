9d5d53c09be812d4f5b32a1296c8f7513f7a102b
= <=>
2024-05-28 10:59:24 -0700

    [feature] GetCompanies Action : Added GetAllCompanies Method in ICompanyService.

--- CompanyEmployees/Service.Contracts/ICompanyService.cs
+++ CompanyEmployees/Service.Contracts/ICompanyService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Service.Contracts
 {
     public interface ICompanyService
     {
+        IEnumerable<Company> GetAllCompanies(bool trackChanges);
     }
 }
