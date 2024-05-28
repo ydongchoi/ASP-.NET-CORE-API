dcfe41baf579a002c155e0c5227336cd4d539b95
= <=>
2024-05-28 11:03:26 -0700

    [feature] GetCompanies Action : Implemented GetAllCompanies in CompanyService.

--- CompanyEmployees/Service/CompanyService.cs
+++ CompanyEmployees/Service/CompanyService.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts;
+using Entities.Models;
 using Service.Contracts;
 
 namespace Service
@@ -12,6 +13,21 @@ namespace Service
         {
             _repository = repository;
             _logger = logger;
+        }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        {
+            try
+            {
+                var companies = _repository.Company.GetAllCompanies(trackChanges);
+
+                return companies;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetAllCompanies)} service method {ex}");
+                throw;
+            }
         }
 
     }
