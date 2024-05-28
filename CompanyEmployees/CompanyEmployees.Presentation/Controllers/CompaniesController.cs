020e222159e213615401c8743a820da3b61de1f2
= <=>
2024-05-28 10:32:54 -0700

    [feature] Created CompaniesControllr Class.

---
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+    }
+}
