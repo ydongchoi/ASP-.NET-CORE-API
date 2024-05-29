9a7566e0419586b33c3a464ef39fa327bcbee06a
ydongchoi <105weapon@gmail.com>
2024-05-29 14:19:13 -0700

    [feature] Implemented CompanyNotFoundException class.

---
+++ CompanyEmployees/Entities/Entities/CompanyNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public sealed class CompanyNotFoundException : NotFoundException
+    {
+        public CompanyNotFoundException(Guid companyId)
+            : base($"The company with id: {companyId} doesn't exist in the database.")
+        {    
+        }
+    }
+}
