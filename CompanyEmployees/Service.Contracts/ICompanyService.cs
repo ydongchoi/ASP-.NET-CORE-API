8413f27e8a79539c32cb77c989fc5fd29698baa9
ydongchoi <105weapon@gmail.com>
2024-05-29 09:48:27 -0700

    [feature] Changed GetAllCompanies Service Return Type<DTO>.

--- CompanyEmployees/Service.Contracts/ICompanyService.cs
+++ CompanyEmployees/Service.Contracts/ICompanyService.cs
@@ -1,4 +1,4 @@
-﻿using Entities.Models;
+﻿using Shared.DataTransferObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +9,6 @@ namespace Service.Contracts
 {
     public interface ICompanyService
     {
-        IEnumerable<Company> GetAllCompanies(bool trackChanges);
+        IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
     }
 }
