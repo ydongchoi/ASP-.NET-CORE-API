d4f80761830a05f9d503bb6257ebf2649a94790e
ydongchoi <105weapon@gmail.com>
2024-05-29 13:59:46 -0700

    [feature] Implemented Get Single Company in Service.

--- CompanyEmployees/Service.Contracts/ICompanyService.cs
+++ CompanyEmployees/Service.Contracts/ICompanyService.cs
@@ -10,5 +10,6 @@ namespace Service.Contracts
     public interface ICompanyService
     {
         IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
+        CompanyDto GetCompany(Guid companyId, bool trackChanges);
     }
 }
--- CompanyEmployees/Service/CompanyService.cs
+++ CompanyEmployees/Service/CompanyService.cs
@@ -27,5 +27,13 @@ namespace Service
             return companiesDto;
         }
 
+        public CompanyDto GetCompany(Guid id, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(id, trackChanges);
+
+            var companyDto = _mapper.Map<CompanyDto>(company);
+            return companyDto;
+        }
+
     }
 }
