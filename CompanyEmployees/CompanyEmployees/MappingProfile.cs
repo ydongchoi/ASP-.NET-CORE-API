706ad65e9d5f3f982f928f0eb98983b3a88993b6
ydongchoi <105weapon@gmail.com>
2024-05-29 15:01:26 -0700

    [feature] Created EmployeeDto and Mapping Rule.

--- CompanyEmployees/CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/CompanyEmployees/MappingProfile.cs
@@ -11,6 +11,8 @@ namespace CompanyEmployees
             CreateMap<Company, CompanyDto>()
                 .ForCtorParam("FullAddress",
                 opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+
+            CreateMap<Employee, EmployeeDto>();
         }
     }
 }
