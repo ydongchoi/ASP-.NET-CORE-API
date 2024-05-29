342e70127400cea29364ccc7d29a1a027cfc0db7
ydongchoi <105weapon@gmail.com>
2024-05-29 14:58:40 -0700

    [feature] Implemented GetEmployees in Repository.

--- CompanyEmployees/Contracts/IEmployeeRepository.cs
+++ CompanyEmployees/Contracts/IEmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Contracts
 {
     public interface IEmployeeRepository
     {
+        IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges);
     }
 }
--- CompanyEmployees/Repository/EmployeeRepository.cs
+++ CompanyEmployees/Repository/EmployeeRepository.cs
@@ -14,5 +14,9 @@ namespace Repository
             : base(repositoryContext)
         {
         }
+
+        public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) =>
+            FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+            .OrderBy(e => e.Name).ToList();
     }
 }
