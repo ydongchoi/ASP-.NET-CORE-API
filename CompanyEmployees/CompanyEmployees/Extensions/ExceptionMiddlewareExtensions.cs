e8adc935016e170bd9d5d5506f60dee3e2d63382
ydongchoi <105weapon@gmail.com>
2024-05-29 14:28:02 -0700

    [feature] Modified ExceptionMiddlewareExtensnions for populating StatusCode based on exception we throw.

--- CompanyEmployees/CompanyEmployees/Extensions/ExceptionMiddlewareExtensions.cs
+++ CompanyEmployees/CompanyEmployees/Extensions/ExceptionMiddlewareExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Entities.ErrorModel;
+using Entities.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 using System.Net;
 
@@ -19,12 +20,18 @@ namespace CompanyEmployees.Extensions
                     var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
                     if (contextFeature != null)
                     {
+                        context.Response.StatusCode = contextFeature.Error switch
+                        {
+                            NotFoundException => StatusCodes.Status404NotFound,
+                            _ => StatusCodes.Status500InternalServerError
+                        };
+
                         logger.LogError($"Something went wrong: {contextFeature.Error}");
 
                         await context.Response.WriteAsync(new ErrorDetails()
                         {
                             StatusCode = context.Response.StatusCode,
-                            Message = "Internal Server Error.",
+                            Message = contextFeature.Error.Message,
                         }.ToString());
                     }
                 });    
