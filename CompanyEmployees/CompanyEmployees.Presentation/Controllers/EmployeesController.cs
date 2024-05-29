1a52fbe4e2f93a3625a584989fc5dd844b811841
ydongchoi <105weapon@gmail.com>
2024-05-29 14:52:14 -0700

    [feature] Created EmployeesController in Presentation Layer.

---
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Presentation.Controllers
+{
+    [Route("api/companies/{companyId}/employees")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public EmployeesController(IServiceManager service) => _service = service;
+        
+    }
+}
