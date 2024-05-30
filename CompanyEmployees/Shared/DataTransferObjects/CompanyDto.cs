645f703e42496841c192647bc3b3a72cf1859ff1
ydongchoi <105weapon@gmail.com>
2024-05-30 10:58:15 -0700

    [feature] Modified CompanyDto Record for protecting the state of the object from mutation.

--- CompanyEmployees/Shared/DataTransferObjects/CompanyDto.cs
+++ CompanyEmployees/Shared/DataTransferObjects/CompanyDto.cs
@@ -6,5 +6,10 @@ using System.Threading.Tasks;
 
 namespace Shared.DataTransferObjects
 {
-    public record CompanyDto(Guid Id, string Name, string FullAddress);
+    public record CompanyDto
+    {
+        public Guid Id { get; init; }
+        public string? Name { get; init; }
+        public string? FullAddress { get; init; }
+    }
 }
