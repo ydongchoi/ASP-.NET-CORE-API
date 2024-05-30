8c99aa3903040f70bfbe48271991186bea517da4
ydongchoi <105weapon@gmail.com>
2024-05-30 11:25:31 -0700

    [feature] Implemented AddCustomCSVFormatter for registering CsvOutputFormatter.

--- CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -35,5 +35,8 @@ namespace CompanyEmployees.Extensions
 
         public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
             services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+            builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
     }
 }
--- CompanyEmployees/CompanyEmployees/Program.cs
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -22,7 +22,8 @@ builder.Services.AddControllers(config => {
     config.RespectBrowserAcceptHeader = true;
     config.ReturnHttpNotAcceptable = true;
 }).AddXmlDataContractSerializerFormatters()
-.AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+  .AddCustomCSVFormatter()
+  .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
 var app = builder.Build();
 
