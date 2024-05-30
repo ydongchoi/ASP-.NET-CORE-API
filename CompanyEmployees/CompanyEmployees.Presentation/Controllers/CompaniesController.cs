a2c83e4f725f430f32a314ff71c3fac679c4e4ac
ydongchoi <105weapon@gmail.com>
2024-05-30 15:08:40 -0700

    [feature] Implemented CreateCompany Action Method in Presentation Layer.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +30,17 @@ namespace CompanyEmployees.Presentation.Controllers
         {
             var company = _service.CompanyService.GetCompany(id, trackChanges: false);
             return Ok(company);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompany([FromBody] CompanyForCreationDto company)
+        {
+            if (company is null)
+                return BadRequest("CompanyForCreationDto object is null.");
+
+            var createdCompany = _service.CompanyService.CreateCompany(company);
+
+            return CreatedAtRoute("CompanyById", new { id = createdCompany.Id }, createdCompany);
         }
 
     }
