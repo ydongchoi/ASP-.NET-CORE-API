dd354743facb4d1f71ec50b876fd3cf231c6db7e
ydongchoi <105weapon@gmail.com>
2024-05-30 11:02:31 -0700

    [feature] Configured Controllers for returning 406 Not Acceptable when client tries to negotiate for the media type the server doesn't support.

--- CompanyEmployees/CompanyEmployees/Program.cs
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddControllers(config => {
     config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
 }).AddXmlDataContractSerializerFormatters()
 .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
