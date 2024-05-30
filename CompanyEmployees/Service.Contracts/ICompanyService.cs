7fc11733f673b9e385c20fd52d93b197ae280e72
ydongchoi <105weapon@gmail.com>
2024-05-30 14:59:20 -0700

    [feature] Implemented CreateCompany in Service Layer.

--- CompanyEmployees/Service.Contracts/ICompanyService.cs
+++ CompanyEmployees/Service.Contracts/ICompanyService.cs
@@ -11,5 +11,6 @@ namespace Service.Contracts
     {
         IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
         CompanyDto GetCompany(Guid companyId, bool trackChanges);
+        CompanyDto CreateCompany(CompanyForCreationDto company);
     }
 }
--- CompanyEmployees/Service/CompanyService.cs
+++ CompanyEmployees/Service/CompanyService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Contracts;
 using Entities.Exceptions;
+using Entities.Models;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 
@@ -37,6 +38,18 @@ namespace Service
 
             var companyDto = _mapper.Map<CompanyDto>(company);
             return companyDto;
+        }
+
+        public CompanyDto CreateCompany(CompanyForCreationDto company)
+        {
+            var companyEntity = _mapper.Map<Company>(company);
+
+            _repository.Company.CreateCompany(companyEntity);
+            _repository.Save();
+
+            var companyToReturn = _mapper.Map<CompanyDto>(companyEntity);
+            
+            return companyToReturn;
         }
 
     }
