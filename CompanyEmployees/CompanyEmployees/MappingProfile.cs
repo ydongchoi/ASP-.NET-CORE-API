dfd376588bbc3259c000559286868890ced4e968
ydongchoi <105weapon@gmail.com>
2024-05-30 15:00:30 -0700

    [feature] Added Mapping Rule for CompanyForCreationDto and Company.

--- CompanyEmployees/CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/CompanyEmployees/MappingProfile.cs
@@ -13,6 +13,8 @@ namespace CompanyEmployees
                 opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
 
             CreateMap<Employee, EmployeeDto>();
+
+            CreateMap<CompanyForCreationDto, Company>();
         }
     }
 }
