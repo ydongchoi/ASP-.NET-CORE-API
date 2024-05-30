1f2b74cfe46574c5b31b5793cc14dc6ab7e12ade
ydongchoi <105weapon@gmail.com>
2024-05-30 10:29:50 -0700

    [feature] Added EmployeeNotFoundException for exception handling.

---
+++ CompanyEmployees/Entities/Exceptions/EmployeeNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+    public class EmployeeNotFoundException : NotFoundException
+    {
+        public EmployeeNotFoundException(Guid employeeId)
+            : base($"Employee with id: {employeeId} doesn't exist in the database.")
+        {            
+        }
+    }
+}
