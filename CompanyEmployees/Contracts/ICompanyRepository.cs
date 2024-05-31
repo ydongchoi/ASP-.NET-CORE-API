40b831cb75f4bb668380b5ea4e5d501e52ff01e6
ydongchoi <105weapon@gmail.com>
2024-05-31 13:28:58 -0700

    [feature] Implemented GetByIds for company in Infrastructure Layer.

--- CompanyEmployees/Contracts/ICompanyRepository.cs
+++ CompanyEmployees/Contracts/ICompanyRepository.cs
@@ -12,5 +12,6 @@ namespace Contracts
         IEnumerable<Company> GetAllCompanies(bool trackChange);
         Company GetCompany(Guid companyId, bool trackChange);
         void CreateCompany(Company company);
+        IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
     }
 }
--- CompanyEmployees/Repository/CompanyRepository.cs
+++ CompanyEmployees/Repository/CompanyRepository.cs
@@ -25,5 +25,9 @@ namespace Repository
             .SingleOrDefault();
 
         public void CreateCompany(Company company) => Create(company);
+
+        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
+            FindByCondition(x => ids.Contains(x.Id), trackChanges)
+            .ToList();
     }
 }
