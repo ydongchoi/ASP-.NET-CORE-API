98c7c5d816ef0a8afa3558e472d6116b7b1baa37
ydongchoi <105weapon@gmail.com>
2024-05-31 13:55:59 -0700

    [feature] Implemented CompanyCollectionBadrequest.

---
+++ CompanyEmployees/Entities/Exceptions/CompanyCollectionBadRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+    public sealed class CompanyCollectionBadRequest : BadRequestException
+    {
+        public CompanyCollectionBadRequest()
+            : base("Company collection sent from a client is null.")
+        {   
+        }
+    }
+}
