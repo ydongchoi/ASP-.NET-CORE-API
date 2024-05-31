d0ccd485fe9783fbecff44f8d9a7a85e151c5ffe
ydongchoi <105weapon@gmail.com>
2024-05-31 14:25:43 -0700

    [feature] Implemented ModelBinder for binding string type parameter to IEnumerable<Guid>.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CompanyEmployees.Presentation.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 using System;
@@ -44,7 +45,7 @@ namespace CompanyEmployees.Presentation.Controllers
         }
 
         [HttpGet("collection/{ids}", Name = "CompanyCollection")]
-        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids) {
+        public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))]IEnumerable<Guid> ids) {
             var companies = _service.CompanyService.GetByIds(ids, trackChanges : false);
             
             return Ok(companies);
