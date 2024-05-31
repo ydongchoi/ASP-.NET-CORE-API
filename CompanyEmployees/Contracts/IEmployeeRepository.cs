0070080748c38d73f289f5f6a5616c873d85fae2
ydongchoi <105weapon@gmail.com>
2024-05-31 11:38:49 -0700

    [feature] Implemented CreateEmployeeForCompany in Infrastructure Layer.

--- CompanyEmployees/Contracts/IEmployeeRepository.cs
+++ CompanyEmployees/Contracts/IEmployeeRepository.cs
@@ -12,5 +12,7 @@ namespace Contracts
         IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges);
 
         Employee GetEmployee(Guid companyId, Guid id, bool trackChanges);
+
+        void CreateEmployeeForCompany(Guid companyId, Employee employee);
     }
 }
--- CompanyEmployees/Repository/EmployeeRepository.cs
+++ CompanyEmployees/Repository/EmployeeRepository.cs
@@ -23,6 +23,10 @@ namespace Repository
             FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
             .SingleOrDefault();
 
-
+        public void CreateEmployeeForCompany(Guid companyId, Employee employee)
+        {
+            employee.CompanyId = companyId;
+            Create(employee);
+        }
     }
 }
