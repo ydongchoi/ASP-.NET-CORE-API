1bab38dc3a249d9162d51bc57e9880150408f40f
ydongchoi <105weapon@gmail.com>
2024-05-31 13:43:30 -0700

    [feature] Created Abstract BadRequest Class and Implemented BadRequest Exception.

--- CompanyEmployees/CompanyEmployees/Extensions/ExceptionMiddlewareExtensions.cs
+++ CompanyEmployees/CompanyEmployees/Extensions/ExceptionMiddlewareExtensions.cs
@@ -23,6 +23,7 @@ namespace CompanyEmployees.Extensions
                         context.Response.StatusCode = contextFeature.Error switch
                         {
                             NotFoundException => StatusCodes.Status404NotFound,
+                            BadRequestException => StatusCodes.Status400BadRequest,
                             _ => StatusCodes.Status500InternalServerError
                         };
 
