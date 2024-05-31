7e24fb5147ee1e19a8b070f145503039eab536b0
ydongchoi <105weapon@gmail.com>
2024-05-31 12:20:55 -0700

    [feature] Extended CompanyForCreationDto for creating children resource together with a parent.

--- CompanyEmployees/Shared/DataTransferObjects/CompanyForCreationDto.cs
+++ CompanyEmployees/Shared/DataTransferObjects/CompanyForCreationDto.cs
@@ -6,5 +6,6 @@ using System.Threading.Tasks;
 
 namespace Shared.DataTransferObjects
 {
-    public record CompanyForCreationDto(string Name, string Address, string Country);
+    public record CompanyForCreationDto(string Name, string Address, string Country,
+        IEnumerable<EmployeeForCreationDto> Employees);
 }
