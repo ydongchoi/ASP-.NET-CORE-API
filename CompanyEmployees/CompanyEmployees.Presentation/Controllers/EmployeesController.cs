bf28eede4bac7f9a2fac7ff3dddb1a984eab4920
ydongchoi <105weapon@gmail.com>
2024-06-01 21:11:20 -0700

    [feature] Implemented DeleteEmployeeForCompany Action Method in Presentation Layer.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -43,6 +43,13 @@ namespace CompanyEmployees.Presentation.Controllers
             return CreatedAtRoute("GetEmployeeForCompany", new { companyId, id = employeeToReturn.Id }, 
                 employeeToReturn);
         }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult DeleteEmployeeForCompany(Guid companyId, Guid id)
+        {
+            _service.EmployeeService.DeleteEmployeeForCompany(companyId, id, trackChanges: false);
+            return NoContent();
+        }
                 
     }
 }
