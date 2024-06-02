527b3059d0050d59b370069e922978bdbd42f8b2
ydongchoi <105weapon@gmail.com>
2024-06-02 16:42:52 -0700

    [feature] Implemented UpdateEmployeeForCompany in Service Layer.

--- CompanyEmployees/Service.Contracts/IEmployeeService.cs
+++ CompanyEmployees/Service.Contracts/IEmployeeService.cs
@@ -16,5 +16,7 @@ namespace Service.Contracts
         EmployeeDto CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto employeeForCreation, bool trackChanges);
 
         void DeleteEmployeeForCompany(Guid companyId, Guid id, bool trackChanges);
+
+        void UpdateEmployeeForCompany(Guid companyId, Guid id, EmployeeForUpdateDto employeeForUpdate, bool compTrackChanges, bool empTrackChanges);
     }
 }
--- CompanyEmployees/Service/EmployeeService.cs
+++ CompanyEmployees/Service/EmployeeService.cs
@@ -81,5 +81,19 @@ namespace Service
             _repository.Save();
         }
 
+        public void UpdateEmployeeForCompany(Guid companyId, Guid id, EmployeeForUpdateDto employeeForUpdate, bool compTrackChanges, bool empTrackChanges)
+        {
+            var company = _repository.Company.GetCompany(companyId, compTrackChanges);
+            if(company is null)
+                throw new CompanyNotFoundException(companyId);
+
+            var employeeEntity = _repository.Employee.GetEmployee(companyId, id, empTrackChanges);
+            if (employeeEntity is null)
+                throw new EmployeeNotFoundException(id);
+
+            _mapper.Map(employeeForUpdate, employeeEntity);
+            _repository.Save();
+        }
+
     }
 }
