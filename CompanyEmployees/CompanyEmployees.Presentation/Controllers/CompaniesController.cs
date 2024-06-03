7997bea31d3b425ed80d54861b90729ea679a9a0
ydongchoi <105weapon@gmail.com>
2024-06-02 22:05:55 -0700

    [feature] Implemented UpdateCompany Action Method in Presentation Layer.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -66,5 +66,16 @@ namespace CompanyEmployees.Presentation.Controllers
             
             return NoContent();
         }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpdateCompany(Guid id, [FromBody] CompanyForUpdateDto company)
+        {
+            if (company is null)
+                return BadRequest("CompanyForUpdateDto object is null.");
+
+            _service.CompanyService.UpdateCompany(id, company, trackChanges: true);
+            
+            return NoContent();
+        }
     }
 }
