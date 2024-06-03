cab0721625b0c2063a94c34e857e4218e24a12f9
ydongchoi <105weapon@gmail.com>
2024-06-03 10:53:22 -0700

    [feature] Modified EmployeeForCreationDto for applying Validation.

--- CompanyEmployees/Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ CompanyEmployees/Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.DataTransferObjects
 {
-    public record EmployeeForCreationDto(string Name, int Age, string Position);
+    public record EmployeeForCreationDto 
+    {
+        [Required(ErrorMessage = "Employee name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string? Name { get; init; }
+
+        [Required(ErrorMessage = "Age is a required field.")]
+        public int Age { get; init; }
+        
+        [Required(ErrorMessage = "Position is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+        public string? Position { get; init; }
+    }
 }
