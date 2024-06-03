e7932bbb3639a6d043a11d244b08d0e8a8a5742b
ydongchoi <105weapon@gmail.com>
2024-06-03 14:22:38 -0700

    [refactor] Refactored Sync to Async Method in Repository Manager of Infrastructure Layer.

--- CompanyEmployees/Contracts/IRepositoryManager.cs
+++ CompanyEmployees/Contracts/IRepositoryManager.cs
@@ -10,6 +10,6 @@ namespace Contracts
     {
         ICompanyRepository Company { get; }
         IEmployeeRepository Employee { get; }
-        void Save();
+        Task SaveAsync();
     }
 }
--- CompanyEmployees/Repository/RepositoryManager.cs
+++ CompanyEmployees/Repository/RepositoryManager.cs
@@ -22,6 +22,6 @@ namespace Repository
 
         public ICompanyRepository Company => _companyRepository.Value;
         public IEmployeeRepository Employee => _employeeRepository.Value;
-        public void Save() => _repositoryContext.SaveChanges();
+        public async Task SaveAsync() => _repositoryContext.SaveChangesAsync();
     }
 }
