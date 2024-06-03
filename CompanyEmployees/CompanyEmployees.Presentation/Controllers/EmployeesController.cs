93b592ff949281c728555bc82849e0f103453cb9
ydongchoi <105weapon@gmail.com>
2024-06-03 13:01:29 -0700

    [feature] Added TryValidateModel to validate the already patched instance.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -80,6 +80,8 @@ namespace CompanyEmployees.Presentation.Controllers
 
             patchDoc.ApplyTo(result.employeeToPatch, ModelState);
 
+            TryValidateModel(result.employeeToPatch);
+
             if (!ModelState.IsValid)
                 return UnprocessableEntity(ModelState);
 
