b7050fb17fc0288f2265c8c61386870174e60da0
ydongchoi <105weapon@gmail.com>
2024-06-03 11:12:14 -0700

    [feature] Modified Data Annotation Attribute for Validating Int Type.

--- CompanyEmployees/Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ CompanyEmployees/Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -13,7 +13,7 @@ namespace Shared.DataTransferObjects
         [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
         public string? Name { get; init; }
 
-        [Required(ErrorMessage = "Age is a required field.")]
+        [Range(18, int.MaxValue, ErrorMessage = "Age is required and it can't be lower than 18.")]
         public int Age { get; init; }
         
         [Required(ErrorMessage = "Position is a required field.")]
