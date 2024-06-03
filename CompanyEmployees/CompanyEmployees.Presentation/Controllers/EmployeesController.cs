9b784c1d1fc679a07a4c11ea20bc90d5cd83ced4
ydongchoi <105weapon@gmail.com>
2024-06-03 12:55:38 -0700

    [feature] Added Validation For PartiallyUpdateEmployeeForCompany Action Method.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -78,7 +78,10 @@ namespace CompanyEmployees.Presentation.Controllers
 
             var result = _service.EmployeeService.GetEmployeeForPatch(companyId, id, compTrackChanges: false, empTrackChanges: true);
 
-            patchDoc.ApplyTo(result.employeeToPatch);
+            patchDoc.ApplyTo(result.employeeToPatch, ModelState);
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
 
             _service.EmployeeService.SaveChangesForPatch(result.employeeToPatch, result.employeeEntity);
 
