6e9c3464d9067e84db0c3b47800d321c9dbac843
ydongchoi <105weapon@gmail.com>
2024-06-03 12:54:57 -0700

    [fix] Modified Access Modifier of EmployeeForManipulationDto from private to public.

--- CompanyEmployees/Shared/DataTransferObjects/EmployeeForManipulationDto.cs
+++ CompanyEmployees/Shared/DataTransferObjects/EmployeeForManipulationDto.cs
@@ -14,7 +14,7 @@ namespace Shared.DataTransferObjects
         public string? Name { get; init; }
 
         [Range(18, int.MaxValue, ErrorMessage = "Age is required and it can't be lower than 18")]
-        int Age { get; init;}
+        public int Age { get; init;}
 
         [Required(ErrorMessage = "Position is a required field.")]
         [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
