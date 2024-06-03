657c274c66bbec21e04c813d23c920eaf37f4112
ydongchoi <105weapon@gmail.com>
2024-06-02 21:49:27 -0700

    [feature] Created CompanyForUpdateDto.

---
+++ CompanyEmployees/Shared/DataTransferObjects/CompanyForUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObjects
+{
+    public record CompanyForUpdateDto(string Name, string Address, string Country, IEnumerable<EmployeeForCreationDto> Employees);
+}
