028500c6e8bf87890a05d226f706d9dde9151691
ydongchoi <105weapon@gmail.com>
2024-06-04 15:47:38 -0700

    [feature] Added property in EmployeeParameters for using search query parameter.

--- CompanyEmployees/Shared/RequestFeature/EmployeeParameters.cs
+++ CompanyEmployees/Shared/RequestFeature/EmployeeParameters.cs
@@ -11,5 +11,7 @@ namespace Shared.RequestFeature
         public uint MinAge { get; set; }
         public uint MaxAge { get; set; } = int.MaxValue;
         public bool ValidAgeRange => MaxAge > MinAge;
+
+        public string? SearchTerm { get; set; }
     }
 }
