84bb778f832bc7a2062a2db975f371c5d07042f9
ydongchoi <105weapon@gmail.com>
2024-06-04 15:25:50 -0700

    [feature] Validated Filter Condition in EmployeeService and Added MaxAgeRangeBadRequestExcception.

---
+++ CompanyEmployees/Entities/Exceptions/MaxAgeRangeBadRequestException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+    public sealed class MaxAgeRangeBadRequestException : BadRequestException
+    {
+        public MaxAgeRangeBadRequestException()
+            : base("Max age can't be less than min age.")
+        {
+            
+        }
+    }
+}
--- CompanyEmployees/Service/EmployeeService.cs
+++ CompanyEmployees/Service/EmployeeService.cs
@@ -28,6 +28,9 @@ namespace Service
 
         public async Task<(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
+            if (!employeeParameters.ValidAgeRange)
+                throw new MaxAgeRangeBadRequestException();
+
             await CheckIfCompanyExists(companyId, trackChanges);
 
             var employeesWithMetaData = await _repository.Employee.GetEmployeesAsync(companyId, employeeParameters, trackChanges);
