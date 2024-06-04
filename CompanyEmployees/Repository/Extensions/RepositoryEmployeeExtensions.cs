4d4b6cead5446417c2ef3b3ad64ec2afd5d4e77d
ydongchoi <105weapon@gmail.com>
2024-06-04 15:51:55 -0700

    [feature] Created RepositoryEmployeeExtensions to update query.

---
+++ CompanyEmployees/Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Extensions
+{
+    public static class RepositoryEmployeeExtensions
+    {
+        public static IQueryable<Employee> FilterEmployees(this IQueryable<Employee> employees, uint minAge, uint maxAge) =>
+            employees.Where(e => (e.Age >= minAge && e.Age <= maxAge));
+
+        public static IQueryable<Employee> Search(this IQueryable<Employee> employees, string searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm))
+                return employees;
+
+            var lowerCaseTerm = searchTerm.Trim().ToLower();
+
+            return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+        }
+    }
+}
