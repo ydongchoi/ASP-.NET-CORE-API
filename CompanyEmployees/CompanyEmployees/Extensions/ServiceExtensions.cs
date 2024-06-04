3551e87eb1ac34abfdb6f16b4272061b83bb7429
ydongchoi <105weapon@gmail.com>
2024-06-04 13:02:30 -0700

    [feature] Enabled Client Application to read new X-Pagination Header.

--- CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -15,7 +15,8 @@ namespace CompanyEmployees.Extensions
                 options.AddPolicy("CorsPolicy", builder =>
                 builder.AllowAnyOrigin()
                 .AllowAnyMethod()
-                .AllowAnyHeader());
+                .AllowAnyHeader()
+                .WithExposedHeaders("X-Pagination"));
             });
 
         public static void ConfigureIISIntegration(this IServiceCollection services) =>
