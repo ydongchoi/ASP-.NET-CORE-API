599206623bda0d2cb372bd06f22c629390db4128
ydongchoi <105weapon@gmail.com>
2024-06-04 15:20:22 -0700

    [feature] Added property to define the query parameters for filtering request.

--- CompanyEmployees/Shared/RequestFeature/EmployeeParameters.cs
+++ CompanyEmployees/Shared/RequestFeature/EmployeeParameters.cs
@@ -8,5 +8,8 @@ namespace Shared.RequestFeature
 {
     public class EmployeeParameters : RequestParameters
     {
+        public uint MinAge { get; set; }
+        public uint MaxAge { get; set; } = int.MaxValue;
+        public bool ValidAgeRange => MaxAge > MinAge;
     }
 }
