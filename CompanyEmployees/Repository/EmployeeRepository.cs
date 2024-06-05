8c196f63ba7bbd11eb561371e30aa3224f9e020c
ydongchoi <105weapon@gmail.com>
2024-06-05 10:31:03 -0700

    [feature] Modified GeyEmployeesAsync in EmployeeRepository for using Sort Extension Method.

--- CompanyEmployees/Repository/EmployeeRepository.cs
+++ CompanyEmployees/Repository/EmployeeRepository.cs
@@ -24,7 +24,7 @@ namespace Repository
             e.CompanyId.Equals(companyId), trackChanges)
             .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
             .Search(employeeParameters.SearchTerm)
-            .OrderBy(e => e.Name)
+            .Sort(employeeParameters.OrderBy)
             .ToListAsync();
 
             return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
