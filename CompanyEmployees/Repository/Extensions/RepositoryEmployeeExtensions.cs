d9c65ee7b400455c1b2a844f5370e4dacdc51184
ydongchoi <105weapon@gmail.com>
2024-06-05 10:38:46 -0700

    [refactor] Extracted a logic that can be resued in the CreateOrderQuery<T> and Used.

--- CompanyEmployees/Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ CompanyEmployees/Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 using System.Text;
 using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
+using Repository.Extensions.Utility;
 
 namespace Repository.Extensions
 {
@@ -30,28 +31,7 @@ namespace Repository.Extensions
             if(string.IsNullOrWhiteSpace(orderByQueryString))
                 return employees.OrderBy(e => e.Name);
 
-            var orderParams = orderByQueryString.Trim().Split(',');
-            var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            var orderQueryBuilder = new StringBuilder();
-
-            foreach(var param in orderParams)
-            {
-                if (string.IsNullOrWhiteSpace(param))
-                    continue;
-
-                var propertyFromQueryName = param.Split(" ")[0];
-                var objectProperty = propertyInfos.FirstOrDefault(pi =>
-                pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
-
-                if (objectProperty == null)
-                    continue;
-
-                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-
-                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
-            }
-
-            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
 
             if (string.IsNullOrWhiteSpace(orderQuery))
                 return employees.OrderBy(e => e.Name);
