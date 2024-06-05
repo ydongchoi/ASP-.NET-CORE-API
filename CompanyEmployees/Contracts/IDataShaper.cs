dffcee283918c4e3b53c0049172bb375b2286ece
ydongchoi <105weapon@gmail.com>
2024-06-05 11:47:09 -0700

    [feature] Created DataShapper class for DataShaping Service.

---
+++ CompanyEmployees/Contracts/IDataShaper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IDataShaper<T>
+    {
+        IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldString);
+
+        ExpandoObject ShapeData(T entity, string fieldsString);
+    }
+}
