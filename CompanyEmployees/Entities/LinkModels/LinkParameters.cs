5260357fa9321a3b45be625533f53c970d57cefa
ydongchoi <105weapon@gmail.com>
2024-06-06 14:32:44 -0700

    [feature] Created LinkParameters Record for trasfering required parameters from controller to service.

---
+++ CompanyEmployees/Entities/LinkModels/LinkParameters.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shared.RequestFeature;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.LinkModels
+{
+    public record LinkParameters(EmployeeParameters EmployeeParameters, HttpContext Context);
+}
