07d7ac616ba99662aa2f3be43fad7b35d0c7c0d9
ydongchoi <105weapon@gmail.com>
2024-06-06 10:06:54 -0700

    [feature] Created LinkResponse in LinkModels.

---
+++ CompanyEmployees/Entities/LinkModels/LinkResponse.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.LinkModels
+{
+    public class LinkResponse
+    {
+        public bool HasLinks { get; set; }
+
+        public List<Entity> ShapedEntities { get; set; }
+
+        public LinkCollectionWrapper<Entity> LinkedEntities { get; set; }
+
+        public LinkResponse()
+        {
+            LinkedEntities = new LinkCollectionWrapper<Entity>();
+            ShapedEntities = new List<Entity>();
+        }
+    }
+}
