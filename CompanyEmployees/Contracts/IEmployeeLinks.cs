c7e00b510169a10149177022078c73f8f5cec044
ydongchoi <105weapon@gmail.com>
2024-06-06 14:29:32 -0700

    [feature] Created EmployeeLinks for implementing HATEOAS.

---
+++ CompanyEmployees/Contracts/IEmployeeLinks.cs
@@ -0,0 +1,16 @@
+﻿using Entities.LinkModels;
+using Microsoft.AspNetCore.Http;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IEmployeeLinks
+    {
+        LinkResponse TryGenerateLinks(IEnumerable<EmployeeDto> employeesDto, string fields, Guid companyId, HttpContext httpContext);
+    }
+}
