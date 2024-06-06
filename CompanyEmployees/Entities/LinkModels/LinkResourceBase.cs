a317365016b08685d2abf99386ad0f5c0359b6b6
ydongchoi <105weapon@gmail.com>
2024-06-06 09:48:44 -0700

    [feature] Created LinkModels for HATEOAS.

---
+++ CompanyEmployees/Entities/LinkModels/LinkResourceBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.LinkModels
+{
+    public class LinkResourceBase
+    {
+        public LinkResourceBase()
+        {}
+
+        public List<Link> Links { get; set; } = new List<Link>();
+    }
+}
