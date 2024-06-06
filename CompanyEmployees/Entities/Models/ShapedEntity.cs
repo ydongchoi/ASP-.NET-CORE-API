a88a78e7a594563889e91e5bb3e0cbc2072de1d5
ydongchoi <105weapon@gmail.com>
2024-06-06 09:56:23 -0700

    [feature] Created Shaped Entity.

---
+++ CompanyEmployees/Entities/Models/ShapedEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class ShapedEntity
+    {
+        public ShapedEntity()
+        {
+            Entity = new Entity();
+        }
+
+        public Guid Id { get; set; }
+        public Entity Entity { get; set; }  
+    }
+}
