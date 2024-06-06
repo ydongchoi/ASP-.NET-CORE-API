636ebeb6c907cd37989dc63ac1efd5b9d43d90e3
ydongchoi <105weapon@gmail.com>
2024-06-06 15:12:07 -0700

    [fix] Changed Package from System.Net to Microsoft.Net.

--- CompanyEmployees/CompanyEmployees.Presentation/ActionFilters/ValidateMediaTypeAttribute.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/ActionFilters/ValidateMediaTypeAttribute.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Net.Http.Headers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
