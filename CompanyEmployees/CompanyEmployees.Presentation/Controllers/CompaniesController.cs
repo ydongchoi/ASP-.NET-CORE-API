b94510a1abf243a7f09670ecaa222f1019d53eb2
ydongchoi <105weapon@gmail.com>
2024-06-07 18:57:50 -0700

    [feature] Implemented GetCompaniesOptions Action Method to request information. available.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -74,5 +74,13 @@ namespace CompanyEmployees.Presentation.Controllers
             
             return NoContent();
         }
+
+        [HttpOptions]
+        public IActionResult GetCompaniesOptions()
+        {
+            Response.Headers.Add("Allow", "GET, OPTIONS, POST, PUT, DELETE");
+
+            return Ok();
+        }
     }
 }
