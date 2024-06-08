404bdc09a1508ba79b87c4a354c2402d940bd929
ydongchoi <105weapon@gmail.com>
2024-06-07 19:48:47 -0700

    [feature] Configured Versioning for HTTP Header Versioning

--- CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -2,6 +2,7 @@
 using LoggerService;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Repository;
 using Service;
@@ -77,6 +78,7 @@ namespace CompanyEmployees.Extensions
                 opt.ReportApiVersions = true;
                 opt.AssumeDefaultVersionWhenUnspecified = true;
                 opt.DefaultApiVersion = new ApiVersion(1, 0);
+                opt.ApiVersionReader = new HeaderApiVersionReader("api-version");
             });
         }
     }
