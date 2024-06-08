b4b1120d758f9ec5103d2e21a3aa290e1e4553fa
ydongchoi <105weapon@gmail.com>
2024-06-07 19:21:39 -0700

    [feature] Implemented RootController for getting links on the URIs at the root level.

---
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/RootController.cs
@@ -0,0 +1,51 @@
+﻿using Entities.LinkModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Presentation.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class RootController: ControllerBase
+    {
+        private readonly LinkGenerator _linkGenerator;
+
+        public RootController(LinkGenerator linkGenerator) => 
+            _linkGenerator = linkGenerator;
+
+        [HttpGet(Name = "GetRoot")]
+        public IActionResult GetRoot([FromHeader(Name = "Accept")] string mediaType)
+        {
+            if(mediaType.Contains("application/vnd.ydong.apiroot"))
+            {
+                var list = new List<Link>{
+                    new Link{
+                        Href = _linkGenerator.GetUriByName(HttpContext, nameof(GetRoot), new{}),
+                        Rel = "self",
+                        Method = "GET"
+                    },
+                    new Link{
+                        Href = _linkGenerator.GetUriByName(HttpContext, "GetCompanies", new{}),
+                        Rel = "companies",
+                        Method = "GET"
+                    },
+                    new Link{
+                        Href = _linkGenerator.GetUriByName(HttpContext, "CreateCompany", new{}),
+                        Rel = "create_company",
+                        Method = "POST"
+                    }
+                 };
+
+                return Ok(list);
+            }
+
+            return NoContent();
+        }
+
+    }
+}
