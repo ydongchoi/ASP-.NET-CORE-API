8868309868db3d1db328cc57f4e37de9af14a313
ydongchoi <105weapon@gmail.com>
2024-06-07 20:22:32 -0700

    [feature] Added Cache Header to GetCompany Action Method in CompaniesController.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -29,6 +29,7 @@ namespace CompanyEmployees.Presentation.Controllers
         }
 
         [HttpGet("{id:guid}", Name = "CompanyById")]
+        [ResponseCache(Duration = 60)]
         public async Task<IActionResult> GetCompany(Guid id)
         {
             var company = await _service.CompanyService.GetCompanyAsync(id, trackChanges: false);
