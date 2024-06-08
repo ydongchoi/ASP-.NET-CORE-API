44df8ef25d4a8f6abad8dbd57467bd769f6fb0b2
ydongchoi <105weapon@gmail.com>
2024-06-07 19:46:27 -0700

    [feature] Implemented Companies Api Version 2.0.

---
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesV2Controller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Presentation.Controllers
+{
+    [ApiVersion("2.0")]
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesV2Controller :ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public CompaniesV2Controller(IServiceManager service) => _service = service;
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await _service.CompanyService.GetAllCompaniesAsync(trackChanges: false);
+
+            return Ok(companies);
+        }
+    }
+}
