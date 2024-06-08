a721c89e2670e6c2b2743776a847ac277d4b0834
ydongchoi <105weapon@gmail.com>
2024-06-07 19:10:50 -0700

    [feature] Added a name fot the action method.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -19,7 +19,7 @@ namespace CompanyEmployees.Presentation.Controllers
 
         public CompaniesController(IServiceManager service) => _service = service;
 
-        [HttpGet]
+        [HttpGet(Name = "GetCompanies")]
         public async Task<IActionResult> GetCompanies()
         {
             var companies = await _service.CompanyService.GetAllCompaniesAsync(trackChanges: false);
@@ -34,7 +34,7 @@ namespace CompanyEmployees.Presentation.Controllers
             return Ok(company);
         }
 
-        [HttpPost]
+        [HttpPost(Name = "CreateCompany")]
         [ServiceFilter(typeof(ValidationFilterAttribute))]
         public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDto company)
         {
