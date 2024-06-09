65fd8ad4d329293c3ea5b357ee48291114ee4a97
ydongchoi <105weapon@gmail.com>
2024-06-09 14:49:13 -0700

    [feature] Created AuthenticationController for create/register a new user.

---
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Presentation.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public AuthenticationController(IServiceManager service) => _service = service;
+    }
+}
