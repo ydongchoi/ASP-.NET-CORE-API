d2b589d44f870e9bb003dc623f71393237fe25fc
ydongchoi <105weapon@gmail.com>
2024-06-09 15:02:19 -0700

    [feature] Created Authentication Service for Reistering User.

---
+++ CompanyEmployees/Service.Contracts/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration);
+    }
+}
---
+++ CompanyEmployees/Service/AuthenticationService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public sealed class AuthenticationService : IAuthenticationService
+    {
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationService(ILoggerManager loger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
+        {
+            _logger = loger;
+            _mapper = mapper;
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration)
+        {
+            var user = _mapper.Map<User>(userForRegistration);
+
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+
+            if(result.Succeeded)
+                await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+
+            return result;
+        }
+
+    }
+}
