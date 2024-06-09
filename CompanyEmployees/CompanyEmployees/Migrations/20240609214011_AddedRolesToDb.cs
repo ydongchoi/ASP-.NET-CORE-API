5c25361a3866b96c017314c53d91ad7c86abb441
ydongchoi <105weapon@gmail.com>
2024-06-09 14:41:04 -0700

    [feature] Add-Migration AddedRolesToDb.

---
+++ CompanyEmployees/CompanyEmployees/Migrations/20240609214011_AddedRolesToDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CompanyEmployees.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRolesToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "207cdbd0-01c3-46f5-88e6-6f6228611331", null, "Administrator", "ADMINISTRATOR" },
+                    { "cb660bb6-b99a-4231-a10d-243cb65d0c4c", null, "Manager", "MANAGER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "207cdbd0-01c3-46f5-88e6-6f6228611331");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cb660bb6-b99a-4231-a10d-243cb65d0c4c");
+        }
+    }
+}
