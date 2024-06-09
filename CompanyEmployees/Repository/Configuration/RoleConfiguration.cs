34243375f74d4907fb3f031fa26ffd87286fa254
ydongchoi <105weapon@gmail.com>
2024-06-09 14:39:41 -0700

    [feature] Created RoleConfiguration and Applied.

---
+++ CompanyEmployees/Repository/Configuration/RoleConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Configuration
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Name = "Manager",
+                    NormalizedName = "MANAGER"
+                },
+                new IdentityRole
+                {
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR"
+                }              
+            );
+        }
+    }
+}
--- CompanyEmployees/Repository/RepositoryContext.cs
+++ CompanyEmployees/Repository/RepositoryContext.cs
@@ -18,6 +18,7 @@ namespace Repository
 
             modelBuilder.ApplyConfiguration(new CompanyConfiguration());
             modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
         }
 
         public DbSet<Company>? Companies { get; set; }
