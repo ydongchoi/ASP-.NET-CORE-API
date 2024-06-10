8663ae8f8f4cc0514bdcf3bb7ae7dbde06c63c21
ydongchoi <105weapon@gmail.com>
2024-06-10 12:25:34 -0700

    [feature] Created Authenticate Action Method for Validating User and Creating Token.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -36,5 +36,15 @@ namespace CompanyEmployees.Presentation.Controllers
 
             return StatusCode(201);
         }
+
+        [HttpPost("login")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> Authenticate([FromBody] UserForAuthenticationDto user)
+        {
+            if (!await _service.AuthenticationService.ValidateUser(user))
+                return Unauthorized();
+
+            return Ok(new { Token = await _service.AuthenticationService.CreateToken() });
+        }
     }
 }
