a41dcc87129cc8373d2c635db0e356251d7e3810
ydongchoi <105weapon@gmail.com>
2024-06-09 21:06:21 -0700

    [feature] Implemented RegisterUser Action Method in AuthenticationController.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CompanyEmployees.Presentation.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,5 +17,24 @@ namespace CompanyEmployees.Presentation.Controllers
         private readonly IServiceManager _service;
 
         public AuthenticationController(IServiceManager service) => _service = service;
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            var result = await _service.AuthenticationService.RegisterUser(userForRegistration);
+
+            if (!result.Succeeded)
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return StatusCode(201);
+        }
     }
 }
