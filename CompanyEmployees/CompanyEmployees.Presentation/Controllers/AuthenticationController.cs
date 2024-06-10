55444e8b7f9850ae2682ac1f09a018f7bcad3013
ydongchoi <105weapon@gmail.com>
2024-06-10 14:21:15 -0700

    [feature] Modified Authenticate Action Method for Refresh Token.

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -44,7 +44,9 @@ namespace CompanyEmployees.Presentation.Controllers
             if (!await _service.AuthenticationService.ValidateUser(user))
                 return Unauthorized();
 
-            return Ok(new { Token = await _service.AuthenticationService.CreateToken() });
+            var tokenDto = _service.AuthenticationService.CreateToken(populateExp: true);
+
+            return Ok(tokenDto);
         }
     }
 }
