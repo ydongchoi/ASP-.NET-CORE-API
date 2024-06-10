5466182e66ff1a3b2f4e3334f157af83c57e507c
ydongchoi <105weapon@gmail.com>
2024-06-10 13:48:01 -0700

    [feature] Added Two Properties in User Class for Refresh Token.

--- CompanyEmployees/Entities/Models/User.cs
+++ CompanyEmployees/Entities/Models/User.cs
@@ -9,7 +9,10 @@ namespace Entities.Models
 {
     public class User : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+
     }
 }
