763ec078533ecf483d74678944ec794204543a12
ydongchoi <105weapon@gmail.com>
2024-06-10 11:58:42 -0700

    [feature] Implemented UserForAuthenticationDto for Authentication.

---
+++ CompanyEmployees/Shared/DataTransferObjects/UserForAuthenticationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObjects
+{
+    public class UserForAuthenticationDto
+    {
+        [Required(ErrorMessage = "User name is required.")]
+        public string? UserName { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        public string? Password { get; set; }
+    }
+}
