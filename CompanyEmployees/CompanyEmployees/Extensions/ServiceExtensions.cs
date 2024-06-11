79983db658d4af7facc07773be074953b16c8a59
ydongchoi <105weapon@gmail.com>
2024-06-11 11:36:46 -0700

    [feature] Added Authroziation Support in Swagger Configuration.

--- CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -189,6 +189,30 @@ namespace CompanyEmployees.Extensions
                     Version = "v2"
                 });
 
+                s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Place to add JWT with Bearer",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                s.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Name = "Bearer",
+                        },
+                         new List<string>()
+                    }
+                });
             });
         }
     }
