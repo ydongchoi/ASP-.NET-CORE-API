ce04baa9a08158729a2e1fb21865c4b8e7a52aa2
ydongchoi <105weapon@gmail.com>
2024-06-10 21:25:31 -0700

    [feature] Implemented RefreshToken Action Method in TokenController.

---
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/TokenController.cs
@@ -0,0 +1,30 @@
+﻿using CompanyEmployees.Presentation.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Presentation.Controllers
+{
+    [Route("api/token")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public TokenController(IServiceManager service) => _service = service;
+
+        [HttpPost("refresh")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> Refresh([FromBody] TokenDto tokenDto)
+        {
+            var tokenDtoToReturn = await _service.AuthenticationService.RefreshToken(tokenDto);
+
+            return Ok(tokenDtoToReturn);
+        }
+    }
+}
