56a6ba5f3d5e9219c0265c0539bac8cb4b82fcbd
ydongchoi <105weapon@gmail.com>
2024-06-11 09:28:06 -0700

    [feature] Created JwtConfiguration Entitiy for binding configuration.

---
+++ CompanyEmployees/Entities/ConfigurationModels/JwtConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.ConfigurationModels
+{
+    public class JwtConfiguration
+    {
+        public string Section { get; set; } = "JwtSettings";
+        public string? ValidIssuer { get; set; }
+        public string? ValidAudience { get; set; }
+        public string? Expires { get; set; }
+
+    }
+}
