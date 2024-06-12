25ee898610fa6860fb81f5f27672d09fb0734fd2
ydongchoi <105weapon@gmail.com>
2024-06-12 14:30:27 -0700

    [test] Implemented UpdateEmployeeForCompany Unit test.

--- CompanyEmployees/Tests/Controller/EmployeesControllerTests.cs
+++ CompanyEmployees/Tests/Controller/EmployeesControllerTests.cs
@@ -132,5 +132,66 @@ namespace Tests.Controller
             Assert.IsType<NoContentResult>(result);
         }
 
+        [Fact]
+        public async Task UpdateEmployeeForCompany_EmployeeNull_BadRequest()
+        {
+            // Arrange
+            var companyId = new Guid("C9D4C053-49B6-410C-BC78-2D54A9991870");
+            Guid id = new Guid("80ABBCA8-664D-4B20-B5DE-024705497D4A");
+            EmployeeForUpdateDto employeeForUpdateDto = null;
+
+            // Act
+            var result = await _controller.UpdateEmployeeForCompany(companyId, id, employeeForUpdateDto);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task UpdateEmployeeForCompany_ModelStateInvalid_UnprocessableEntity()
+        {
+            // Arrange
+            var companyId = new Guid("C9D4C053-49B6-410C-BC78-2D54A9991870");
+            Guid id = new Guid("80ABBCA8-664D-4B20-B5DE-024705497D4A");
+            EmployeeForUpdateDto employeeForUpdateDto = new EmployeeForUpdateDto
+            {
+                Name = "Yeongdong",
+                Age = 31
+            };
+
+            _controller.ModelState.AddModelError("Position", "Required");
+
+            // Act
+            var result = await _controller.UpdateEmployeeForCompany(companyId, id, employeeForUpdateDto);
+
+            // Assert
+            Assert.IsType<UnprocessableEntityObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task UpdateEmployeeForCompany_RequestEligibleEmployeeForUpdate_UnprocessableEntity()
+        {
+            // Arrange
+            var companyId = new Guid("C9D4C053-49B6-410C-BC78-2D54A9991870");
+            Guid id = new Guid("80ABBCA8-664D-4B20-B5DE-024705497D4A");
+            EmployeeForUpdateDto employeeForUpdateDto = new EmployeeForUpdateDto
+            {
+                Name = "Yeongdong",
+                Age = 31,
+                Position = "Administrator"
+            };
+            bool compTrackChange = false; bool empTrackChange = false;
+
+            _mockEmployeeService
+                .Setup(m => m.UpdateEmployeeForCompanyAsync(companyId, id, employeeForUpdateDto, compTrackChange, empTrackChange))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _controller.UpdateEmployeeForCompany(companyId, id, employeeForUpdateDto);
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);  
+        }
+
     }
 }
