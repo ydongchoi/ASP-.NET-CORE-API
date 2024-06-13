cc5a53cf7130bf3a0328fb8b3b2d00aa1da9d1c6
ydongchoi <105weapon@gmail.com>
2024-06-13 16:53:20 -0700

    [feature] Created ApiBaseResponse.

---
+++ CompanyEmployees/Entities/Models/ApiBaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public abstract class ApiBaseResponse
+    {
+        public bool Success { get; set; }
+
+        protected ApiBaseResponse(bool success) => Success = success;
+    }
+}
