78c3cb53cad5e3afcb5a8ddb51f7cbed592f20b3
ydongchoi <105weapon@gmail.com>
2024-06-13 12:25:40 -0700

    [test] Applied MockIEmployeeService in Service Manager.

--- CompanyEmployees/Tests/Mocks/MockServiceManager.cs
+++ CompanyEmployees/Tests/Mocks/MockServiceManager.cs
@@ -13,12 +13,13 @@ namespace Tests.Mocks
         public static Mock<IServiceManager> GetMock()
         {
             var mock = new Mock<IServiceManager>();
+            var employeeServiceMock = MockIEmployeeService.GetMock();
 
             // Setup the mock
             mock.Setup(m => m.CompanyService)
                 .Returns(new Mock<ICompanyService>().Object);
             mock.Setup(m => m.EmployeeService)
-                .Returns(new Mock<IEmployeeService>().Object);
+                .Returns(() => employeeServiceMock.Object);
             mock.Setup(m => m.AuthenticationService)
                 .Returns(new Mock<IAuthenticationService>().Object);
 
