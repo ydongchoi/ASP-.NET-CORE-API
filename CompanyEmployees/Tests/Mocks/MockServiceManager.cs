a404e28456a912486ae4dbf51efe18edf750ff93
ydongchoi <105weapon@gmail.com>
2024-06-13 11:14:36 -0700

    [Test] Created MockServiceManager for unit test.

---
+++ CompanyEmployees/Tests/Mocks/MockServiceManager.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Mocks
+{
+    internal class MockServiceManager
+    {
+        public static Mock<IServiceManager> GetMock()
+        {
+            var mock = new Mock<IServiceManager>();
+
+            // Setup the mock
+            mock.Setup(m => m.CompanyService)
+                .Returns(new Mock<ICompanyService>().Object);
+            mock.Setup(m => m.EmployeeService)
+                .Returns(new Mock<IEmployeeService>().Object);
+            mock.Setup(m => m.AuthenticationService)
+                .Returns(new Mock<IAuthenticationService>().Object);
+
+            return mock;
+        }
+    }
+}
