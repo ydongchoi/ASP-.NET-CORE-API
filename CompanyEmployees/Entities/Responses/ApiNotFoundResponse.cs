918cbb12454d202a1c8a3cda8615ecf0457858e5
ydongchoi <105weapon@gmail.com>
2024-06-13 17:07:23 -0700

    [feature] Created ApiNotFoundResponse.

---
+++ CompanyEmployees/Entities/Responses/ApiNotFoundResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Responses
+{
+    public sealed class ApiNotFoundResponse : ApiBaseResponse
+    {
+        public string Message { get; set; }
+
+        public ApiNotFoundResponse(string message)
+            : base(false)
+        {
+            Message = message;
+        }
+    }
+}
