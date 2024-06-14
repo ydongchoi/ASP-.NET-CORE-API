19e103ed6d594ee6bd1ccb9cb241aa54c7ecfb92
ydongchoi <105weapon@gmail.com>
2024-06-13 17:52:17 -0700

    [feature] Modified CompaniesController to use ApiResponse.

---
+++ CompanyEmployees/CompanyEmployees.Presentation/Extensions/ApiBaseResponseExtenstions.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Presentation.Extensions
+{
+    public static class ApiBaseResponseExtenstions
+    {
+        public static TResultType GetResult<TResultType>(this ApiBaseResponse apiBaseResponse) =>
+            ((ApiOkResponse<TResultType>)apiBaseResponse).Result;
+    }
+}
