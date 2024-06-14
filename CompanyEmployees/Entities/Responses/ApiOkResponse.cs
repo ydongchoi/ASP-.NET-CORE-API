f36a5bf540b478f1218f22a75280b2edccf42e14
ydongchoi <105weapon@gmail.com>
2024-06-13 17:05:14 -0700

    [feature] Created ApiOkResponse.

---
+++ CompanyEmployees/Entities/Responses/ApiOkResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Responses
+{
+    public sealed class ApiOkResponse<TResult> : ApiBaseResponse
+    {
+        public TResult Result { get; set; }
+
+        public ApiOkResponse(TResult result)
+            : base(true)
+        {
+            Result = result;
+        }
+    }
+}
