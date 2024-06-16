8716bd5405597326498c939b875dfd50f0810a7d
ydongchoi <105weapon@gmail.com>
2024-06-15 21:02:20 -0700

    [test] Created MockIAuthenticationService.

---
+++ CompanyEmployees/Tests/Mocks/MockIAuthenticationService.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Mocks
+{
+    internal class MockIAuthenticationService
+    {
+        public static Mock<IAuthenticationService> GetMock()
+        {
+            var mock = new Mock<IAuthenticationService>();
+
+            // Setup the mock
+            mock.Setup(m => m.RegisterUser(It.IsAny<UserForRegistrationDto>()))
+                .Callback(() =>
+                {
+                    return;
+                });
+
+            mock.Setup(m => m.ValidateUser(It.IsAny<UserForAuthenticationDto>()))
+                .Callback(() =>
+                {
+                    return;
+                });
+
+            return mock;
+        }
+    }
+}
