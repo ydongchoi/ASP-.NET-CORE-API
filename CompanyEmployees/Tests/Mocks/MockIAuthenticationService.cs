0a59a1485b5120e4bda979b04368aff50f624f02
ydongchoi <105weapon@gmail.com>
2024-06-16 15:20:43 -0700

    [test] Implemented MockIAuthenticationService for AuthenticationController Unit tests.

--- CompanyEmployees/Tests/Mocks/MockIAuthenticationService.cs
+++ CompanyEmployees/Tests/Mocks/MockIAuthenticationService.cs
@@ -1,10 +1,13 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,13 +20,31 @@ namespace Tests.Mocks
             var mock = new Mock<IAuthenticationService>();
 
             // Setup the mock
+            var user = new UserForAuthenticationDto()
+            {
+                UserName = "hello",
+                Password = "hello1234",
+            };
+
             mock.Setup(m => m.RegisterUser(It.IsAny<UserForRegistrationDto>()))
-                .Callback(() =>
+                .ReturnsAsync(() =>
                 {
-                    return;
+                    return IdentityResult.Success;
                 });
 
             mock.Setup(m => m.ValidateUser(It.IsAny<UserForAuthenticationDto>()))
+                .ReturnsAsync((UserForAuthenticationDto userForAuth) =>
+                {
+                    if (userForAuth.UserName == user.UserName && userForAuth.Password == user.Password) return true;
+                    else return false;
+                });
+
+            mock.Setup(m => m.CreateToken(It.IsAny<bool>()))
+                .Callback(() => {
+                    return;
+                });
+
+            mock.Setup(m => m.RefreshToken(It.IsAny<TokenDto>()))
                 .Callback(() =>
                 {
                     return;
