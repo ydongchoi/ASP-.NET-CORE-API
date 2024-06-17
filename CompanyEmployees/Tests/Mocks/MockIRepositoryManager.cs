a00c9770e053baf6b7af62ef674a06de336078b8
ydongchoi <105weapon@gmail.com>
2024-06-17 13:16:59 -0700

    [test] Implemented MockIRepositoryManager for unit testing to Service.

---
+++ CompanyEmployees/Tests/Mocks/MockIRepositoryManager.cs
@@ -0,0 +1,33 @@
+﻿using Contracts;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Mocks
+{
+    internal class MockIRepositoryManager
+    {
+        public static Mock<IRepositoryManager> GetMock()
+        {
+            var mock = new Mock<IRepositoryManager>();
+
+            // Set up the mock
+            mock.Setup(m => m.Company)
+                .Returns(() => new Mock<ICompanyRepository>());
+
+            mock.Setup(m => m.Employee)
+                .Returns(() => new Mock<IEmployeeRepository>());
+
+            mock.Setup(m => m.SaveAsync())
+                .Callback(() =>
+                {
+                    return;
+                });
+
+            return mock;
+        }
+    }
+}
