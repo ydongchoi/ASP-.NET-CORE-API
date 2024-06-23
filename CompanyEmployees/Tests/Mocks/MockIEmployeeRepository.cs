96810cd847ad84166c0d8cea5d9ccae3d24914be
ydongchoi <105weapon@gmail.com>
2024-06-22 21:14:24 -0700

    [test] Created Plain Mock for EmployeeRepository.

---
+++ CompanyEmployees/Tests/Mocks/MockIEmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Mocks
+{
+    internal class MockIEmployeeRepository
+    {
+        public static Mock<IEmployeeRepository> GetMock()
+        {
+            var mock = new Mock<IEmployeeRepository>();
+
+            // Set up the mock
+
+            return mock;
+        }
+    }
+}
