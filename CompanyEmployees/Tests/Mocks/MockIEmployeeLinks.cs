1f9543c514c0dacd4583ede204cf62c58ef15024
ydongchoi <105weapon@gmail.com>
2024-06-25 12:46:01 -0700

    [test] Added MockIEmployeeLinks for EmployeeService unit test.

---
+++ CompanyEmployees/Tests/Mocks/MockIEmployeeLinks.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Mocks
+{
+    internal class MockIEmployeeLinks
+    {
+        public static Mock<IEmployeeLinks> GetMock()
+        {
+            var mock = new Mock<IEmployeeLinks>();
+
+            // Set up the mock
+            mock.Setup(m => m.TryGenerateLinks(It.IsAny<IEnumerable<EmployeeDto>>(), It.IsAny<string>(), It.IsAny<Guid>(), It.IsAny<HttpContext>()))
+                .Returns(() =>
+                {
+                    return;
+                });
+
+            return mock;
+        }
+    }
+}
