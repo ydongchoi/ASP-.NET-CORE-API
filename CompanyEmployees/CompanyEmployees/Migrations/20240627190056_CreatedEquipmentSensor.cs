289d0ac73ee6e7dbf252f3af86696b74be18f8d5
ydongchoi <105weapon@gmail.com>
2024-06-27 12:06:04 -0700

    [feature] Executed Add-Migration for migrating database.

---
+++ CompanyEmployees/CompanyEmployees/Migrations/20240627190056_CreatedEquipmentSensor.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CompanyEmployees.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedEquipmentSensor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "034976af-59ad-424d-b603-a96510a40e4d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3d41f1e9-7976-49b6-a213-4b07e504349d");
+
+            migrationBuilder.CreateTable(
+                name: "Equipments",
+                columns: table => new
+                {
+                    EquipmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipments", x => x.EquipmentId);
+                    table.ForeignKey(
+                        name: "FK_Equipments_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sensors",
+                columns: table => new
+                {
+                    SensorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EquipmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sensors", x => x.SensorId);
+                    table.ForeignKey(
+                        name: "FK_Sensors_Equipments_EquipmentId",
+                        column: x => x.EquipmentId,
+                        principalTable: "Equipments",
+                        principalColumn: "EquipmentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "72016062-94dd-443f-9131-1f4e1d0ba1a2", null, "Administrator", "ADMINISTRATOR" },
+                    { "9fb98f46-d0ab-46c6-b4ad-58e366cb7e97", null, "Manager", "MANAGER" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipments_CompanyId",
+                table: "Equipments",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sensors_EquipmentId",
+                table: "Sensors",
+                column: "EquipmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sensors");
+
+            migrationBuilder.DropTable(
+                name: "Equipments");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "72016062-94dd-443f-9131-1f4e1d0ba1a2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9fb98f46-d0ab-46c6-b4ad-58e366cb7e97");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "034976af-59ad-424d-b603-a96510a40e4d", null, "Administrator", "ADMINISTRATOR" },
+                    { "3d41f1e9-7976-49b6-a213-4b07e504349d", null, "Manager", "MANAGER" }
+                });
+        }
+    }
+}
