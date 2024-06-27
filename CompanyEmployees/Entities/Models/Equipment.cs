766ed41dcad371f93380e34c501c41c2737a05a6
ydongchoi <105weapon@gmail.com>
2024-06-27 11:14:35 -0700

    [feature] Created Equipment, Sensor Entities.

---
+++ CompanyEmployees/Entities/Models/Equipment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Equipment
+    {
+        [Column("EquipmentId")]
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Equipment name is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Name is 20 characters.")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Status is a required field.")]
+        public string Status { get; set; }
+
+        [ForeignKey(nameof(Company))]
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; }
+        
+        public ICollection<Sensor> Sensors { get; set; }    
+    }
+}
---
+++ CompanyEmployees/Entities/Models/Sensor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Sensor
+    {
+        [Column("SensorId")]
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Sensor name is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Name is 20 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Status is a required field.")]
+        public string Type { get; set; }
+
+        [ForeignKey(nameof(Equipment))]
+        public Guid EquipmentId { get; set; }
+
+        public Equipment? equipment { get; set; }
+    }
+}
