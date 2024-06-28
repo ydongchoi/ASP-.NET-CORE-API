cd3a17044288342abacba638793968816c15bc38
ydongchoi <105weapon@gmail.com>
2024-06-28 14:31:20 -0700

    [feature] Created EquipmentConfiguration for Data Seeding.

---
+++ CompanyEmployees/Repository/Configuration/EquipmentConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Configuration
+{
+    public class EquipmentConfiguration : IEntityTypeConfiguration<Equipment>
+    {
+        public void Configure(EntityTypeBuilder<Equipment> builder)
+        {
+            builder.HasData(
+                new Equipment
+                {
+                    Id = new Guid("d2d4c053-49b6-410c-bc78-2d54a9991825"),
+                    Name = "Equip1",
+                    Status = "InOperation",
+                    CompanyId = new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870")
+                }, 
+                new Equipment
+                {
+                    Id = new Guid("a3e4c053-49b6-410c-bc78-2d54a9991823"),
+                    Name = "Equip2",
+                    Status = "Stop",
+                    CompanyId = new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870")
+                }
+            );
+        }
+    }
+}
