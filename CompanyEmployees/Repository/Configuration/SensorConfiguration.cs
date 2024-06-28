d1c82a84450d9579e47918d0519fab8f6391f0b6
ydongchoi <105weapon@gmail.com>
2024-06-28 14:31:42 -0700

    [feature] Created SensorConfiguration for Data Seeding.

---
+++ CompanyEmployees/Repository/Configuration/SensorConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Configuration
+{
+    public class SensorConfiguration : IEntityTypeConfiguration<Sensor>
+    {
+        public void Configure(EntityTypeBuilder<Sensor> builder)
+        {
+            builder.HasData(
+                new Sensor
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Sensor1",
+                    Type = "Pressure",
+                    EquipmentId = new Guid("d2d4c053-49b6-410c-bc78-2d54a9991825")
+                }
+            );
+        }
+    }
+}
