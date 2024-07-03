5a95fa279bad6da2cfeb684063136231d242d85e
ydongchoi <105weapon@gmail.com>
2024-07-03 12:16:11 -0700

    [test] Created CreateEmployeeForCompanyAsync Unit test in EmployeeService.

--- CompanyEmployees/Tests/Service/EmployeeServiceTests.cs
+++ CompanyEmployees/Tests/Service/EmployeeServiceTests.cs
@@ -2,9 +2,11 @@
 using CompanyEmployees;
 using CompanyEmployees.Utility;
 using Contracts;
+using Entities.Exceptions;
 using LoggerService;
 using Moq;
 using Service;
+using Shared.DataTransferObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +32,44 @@ namespace Tests.Service
             _employeeLinks = MockIEmployeeLinks.GetMock();
 
             _service = new EmployeeService(_mockRepository.Object, _logger, _mapper, _employeeLinks.Object);
+        }
+
+        [Fact]
+        public async Task CreateEmployeeForCompanyAsync_Employee_ThenCreatedResult()
+        {
+            // Arrange
+            var companyId = Guid.Parse("43585C00-2346-4FEA-AA74-08DC81A68D90");
+            var employee = new EmployeeForCreationDto()
+            {
+                Name = "yeongdong",
+                Age = 30,
+                Position = "SW Developer"
+            };
+            bool trackChanges = false;
+
+            // Act
+            var result = await _service.CreateEmployeeForCompanyAsync(companyId, employee, trackChanges);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<EmployeeDto>(result);
+        }
+
+        [Fact]
+        public async Task CreateEmployeeForCompanyAsync_NonCompany_ThenCreatedResult()
+        {
+            // Arrange
+            var companyId = Guid.Parse("12585C00-2346-4FEA-AA74-08DC81A68D90");
+            var employee = new EmployeeForCreationDto()
+            {
+                Name = "yeongdong",
+                Age = 30,
+                Position = "SW Developer"
+            };
+            bool trackChanges = false;
+
+            // Act & Assert
+            Assert.ThrowsAsync<CompanyNotFoundException>(async () => await _service.CreateEmployeeForCompanyAsync(companyId, employee, trackChanges));
         }
 
         public IMapper GetMapper()
