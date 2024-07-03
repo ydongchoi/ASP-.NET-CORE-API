1cfee80d21b9865cc10addae436da495175c3f59
ydongchoi <105weapon@gmail.com>
2024-07-03 12:33:41 -0700

    [test] Created Update, DeleteEmployeeForCompanyAsync Unit test.

--- CompanyEmployees/Tests/Service/EmployeeServiceTests.cs
+++ CompanyEmployees/Tests/Service/EmployeeServiceTests.cs
@@ -72,6 +72,68 @@ namespace Tests.Service
             Assert.ThrowsAsync<CompanyNotFoundException>(async () => await _service.CreateEmployeeForCompanyAsync(companyId, employee, trackChanges));
         }
 
+        [Fact]
+        public async Task DeleteEmployeeForCompanyAsync_NonExistingCompany_CompanyNotFoundException()
+        {
+            // Arrange
+            var companyId = Guid.Parse("12585C00-2346-4FEA-AA74-08DC81A68D90");
+            var employeeId = Guid.Parse("80ABBCA8-664D-4B20-B5DE-024705497D4A");
+            bool trackChanges = false;
+
+            // Act & Assert
+            Assert.ThrowsAsync<CompanyNotFoundException>(async () => await _service.DeleteEmployeeForCompanyAsync(companyId, employeeId, trackChanges));
+        }
+
+        [Fact]
+        public async Task DeleteEmployeeForCompanyAsync_NonExistingEmployee_EmployeeNotFoundException()
+        {
+            // Arrange
+            var companyId = Guid.Parse("43585C00-2346-4FEA-AA74-08DC81A68D90");
+            var employeeId = Guid.Parse("77ABBCA8-664D-4B20-B5DE-024705497D4A");
+            bool trackChanges = false;
+
+            // Act & Assert
+            Assert.ThrowsAsync<EmployeeNotFoundException>(async () => await _service.DeleteEmployeeForCompanyAsync(companyId, employeeId, trackChanges));
+        }
+
+        [Fact]
+        public async Task UpdateEmployeeForCompanyAsync_NonExistingCompany_CompanyNotFoundException()
+        {
+            // Arrange
+            var companyId = Guid.Parse("12585C00-2346-4FEA-AA74-08DC81A68D90");
+            var employeeId = Guid.Parse("80ABBCA8-664D-4B20-B5DE-024705497D4A");
+            var employeeForUpdate = new EmployeeForUpdateDto()
+            {
+                Name = "yeongdong",
+                Age = 30,
+                Position = "SW Developer"
+            };
+            bool compTrackChanges = false;
+            bool empTrackChanges = false;
+
+            // Act & Assert
+            Assert.ThrowsAsync<CompanyNotFoundException>(async () => await _service.UpdateEmployeeForCompanyAsync(companyId, employeeId, employeeForUpdate, compTrackChanges, empTrackChanges));
+        }
+
+        [Fact]
+        public async Task UpdateEmployeeForCompanyAsync_NonExistingEmployee_EmployeeNotFoundException()
+        {
+            // Arrange
+            var companyId = Guid.Parse("43585C00-2346-4FEA-AA74-08DC81A68D90");
+            var employeeId = Guid.Parse("77ABBCA8-664D-4B20-B5DE-024705497D4A");
+            var employeeForUpdate = new EmployeeForUpdateDto()
+            {
+                Name = "yeongdong",
+                Age = 30,
+                Position = "SW Developer"
+            };
+            bool compTrackChanges = false;
+            bool empTrackChanges = false;
+
+            // Act & Assert
+            Assert.ThrowsAsync<EmployeeNotFoundException>(async () => await _service.UpdateEmployeeForCompanyAsync(companyId, employeeId, employeeForUpdate, compTrackChanges, empTrackChanges));
+        }
+
         public IMapper GetMapper()
         {
             var mapplingProfile = new MappingProfile();
