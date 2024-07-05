1b79d7d21d6132b61c93fe6c2c61ccec7c80d463
ydongchoi <105weapon@gmail.com>
2024-07-05 13:49:09 -0700

    [feature] Created SensorRepository.

---
+++ CompanyEmployees/Contracts/ISensorRepository.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface ISensorRepository
+    {
+        Task<IEnumerable<Sensor>> GetAllSensorsAsync(Guid equipmentId, bool trackChanges);
+        
+        Task<Sensor> GetSensorAsync(Guid equipmentId, Guid sensorId, bool trackChange);
+
+        void CreateSensorForEquipment(Guid equipmentId, Sensor sensor);
+
+        void DeleteSensor(Sensor sensor);
+    }
+}
---
+++ CompanyEmployees/Repository/SensorRepository.cs
@@ -0,0 +1,36 @@
+﻿using Contracts;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class SensorRepository : RepositoryBase<Sensor>, ISensorRepository
+    {
+        public SensorRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {    
+        }
+
+        public async Task<IEnumerable<Sensor>> GetAllSensorsAsync(Guid equipmentId, bool trackChanges) =>
+            await FindByCondition(s => s.EquipmentId.Equals(equipmentId), trackChanges)
+            .OrderBy(s => s.Name)
+            .ToListAsync();
+
+        public async Task<Sensor> GetSensorAsync(Guid equipmentId, Guid sensorId, bool trackChange) =>
+            await FindByCondition(s => s.EquipmentId.Equals(equipmentId) && s.Id.Equals(sensorId), trackChange)
+            .SingleOrDefaultAsync();
+
+        public void CreateSensorForEquipment(Guid equipmentId, Sensor sensor)
+        {
+            sensor.EquipmentId = equipmentId;
+            Create(sensor);
+        }
+
+        public void DeleteSensor(Sensor sensor) => Delete(sensor);
+    }
+}
