2a68953200bd30498b9aeacec8ec727b9d80ecc4
ydongchoi <105weapon@gmail.com>
2024-07-08 18:30:43 -0700

    [feature] Create ISensorService. for CRUD.

---
+++ CompanyEmployees/Service.Contracts/ISensorService.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Responses;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Contracts
+{
+    public interface ISensorService
+    {
+        Task<ApiBaseResponse> GetAllSensorsAsync(Guid equipmentId, bool trackChanges);
+        
+        Task<ApiBaseResponse> GetSensorAsync(Guid equipmentId, Guid id, bool trackChanges);
+
+        Task<SensorDto> CreateSensorForEquipmentAsync(Guid equipmentId, SensorForCreationDto sensor,bool trackChanges);
+
+        Task DeleteSensorForEquipmentAsync(Guid equipmentId, Guid id, bool trackChanges);
+
+        Task UpdateSensorForEquipmentAsync(Guid equipmentId, Guid id, SensorForUpdateDto sensor, bool cmpTrackChanges, bool empTrackChanges);
+    }
+}
