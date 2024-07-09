237f53aee5177bdf01de5321a76943d25ce998a5
ydongchoi <105weapon@gmail.com>
2024-07-08 18:20:03 -0700

    [feature] Created Sensor Dto For Create, Update, Manipulation.

---
+++ CompanyEmployees/Shared/DataTransferObjects/SensorForManipulationDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObjects
+{
+    public abstract record SensorForManipulationDto
+    {
+        [Required(ErrorMessage = "Sensor name is a required field.")]]
+        public string Name { get; init; }
+
+        [Required(ErrorMessage = "Sensor Type is a required field.")]
+        public string Type { get; init; }
+    }
+}
